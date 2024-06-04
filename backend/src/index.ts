11ba8722f9824d64eb5b5f011ba747cc88802c92
hrysdev <hrysdev@gmail.com>
2024-06-04 14:23:15 +0900

    feat: @apollo/serverを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

---
+++ backend/src/index.ts
@@ -0,0 +1,45 @@
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { addResolversToSchema } from "@graphql-tools/schema";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const schema = loadSchemaSync("./schema.graphql", {
+  loaders: [new GraphQLFileLoader()],
+});
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ schema: schemaWithResolvers });
+
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
