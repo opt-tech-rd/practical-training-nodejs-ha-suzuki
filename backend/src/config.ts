b661bbc9a65019e201a77e6cbbc0b4e63b0cd896
hrysdev <hrysdev@gmail.com>
2024-06-05 18:05:49 +0900

    feat: IDトークンの検証を追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const config = {
   firebase: {
-    serviceAccount: JSON.parse(process.env.GCLOUD_SERVICE_KEY),
+    serviceAccount: process.env.GCLOUD_SERVICE_KEY,
   },
   server: {
     port: 8080,
