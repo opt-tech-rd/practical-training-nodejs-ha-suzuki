71c01406c4a70c5e9528bbce80bbddd7a358ffc0
hrysdev <hrysdev@gmail.com>
2024-06-11 13:55:42 +0900

    feat: マイグレーションのデプロイを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -13,6 +13,6 @@ export const config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
+    database: process.env.DB_NAME,
   },
 };
